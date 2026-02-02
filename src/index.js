6e2642db7a1112b7693d3744a5b925d0fbba6d96
Maksudur7 <maksudurrahamanmishu7@gmail.com>
2026-02-02 22:47:29 +0600

    Final fix for prisma and vercel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-const app = require('./src/app');
-const port = process.env.PORT || 5000;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-app.get("/", (req, res) => {
-  res.send("MediStore Backend is running successfully on Vercel!");
+const app = express();
+app.use(cors({
+    origin: process.env.APP_URL || "https://medistore-woad.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+}))
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('MediStore Backend is running successfully!');
 });
 
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
 
 module.exports = app;
